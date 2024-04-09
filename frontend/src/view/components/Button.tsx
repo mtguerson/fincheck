704cf51a00f2269fb11bd1d8c4688c8e20f73647
Matheus <mtguerson@gmail.com>
2024-04-09 15:44:45 -0400

    creating Dashboard

---
+++ frontend/src/view/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from "react";
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed
+      px-6 h-12 rounded-2xl font-medium text-white transition-all"
+    />
+  );
+}
