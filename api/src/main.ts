27d6e202cacf1695aaa370c6ccb120540a0c13dc
Matheus <mtguerson@gmail.com>
2024-04-10 16:50:11 -0400

    adding cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({ origin: 'http://localhost:5173' }));
 
   app.useGlobalPipes(new ValidationPipe());
 
