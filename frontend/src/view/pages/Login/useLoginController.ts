9eb22ea63d02ca49662fca77a83223fe421c9557
Matheus <mtguerson@gmail.com>
2024-04-10 16:52:06 -0400

    making the requisition to api to create account

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { httpClient } from '../../../app/services/httpClient';
 
 const schema = z.object({
   email: z.string().min(1, "E-mail é obrigatório").email("Informe um e-mail válido"),
@@ -18,8 +19,8 @@ export function useLoginController() {
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log("Chama a Api com:", data);
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    await httpClient.post('auth/signin', data);
   });
 
   return { handleSubmit, register, errors };
--- frontend/src/view/pages/Register/useRegisterController.ts
+++ frontend/src/view/pages/Register/useRegisterController.ts
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { authService } from "../../../app/services/authService";
 
 const schema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
@@ -19,8 +20,8 @@ export function useRegisterController() {
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormSubmit((data) => {
-    console.log(data);
+  const handleSubmit = hookFormSubmit(async (data) => {
+    const { accessToken } = await authService.signup(data);
   })
 
   return { register, errors, handleSubmit };
