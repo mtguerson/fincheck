85b67ed71448e24063c14f04cb4b59887fd19930
Matheus <mtguerson@gmail.com>
2024-04-17 17:42:40 -0400

    creating transaction page header

--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,7 +1,27 @@
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
+import { FilterIcon } from "../../../../components/icons/FilterIcon";
+
 export function Transactions() {
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
-      Transactions
+      <header className="">
+        <div className="flex items-center justify-between">
+          <button className="flex items-center gap-2">
+            <TransactionsIcon />
+            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
+            <ChevronDownIcon className="text-gray-900" />
+          </button>
+
+          <button>
+            <FilterIcon />
+          </button>
+        </div>
+      </header>
+
+      <div className="mt-4">
+        Conteúdo
+      </div>
     </div>
   );
 }
