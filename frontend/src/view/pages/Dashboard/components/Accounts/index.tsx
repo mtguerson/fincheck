0e9a64e39019b745283293bc4c8a9f1a64fb9567
Matheus <mtguerson@gmail.com>
2024-04-22 11:48:03 -0400

    creating transactions cards

--- frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -5,6 +5,7 @@ import { EyeIcon } from "../../../../components/icons/EyeIcon";
 import { AccountCard } from "./AccountCard";
 import { SliderNavigation } from "./SliderNavigation";
 import { UseAccountsController } from "./useAccountsController";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
 
 export function Accounts() {
   const { sliderState, setSliderState, windowWidth } = UseAccountsController();
@@ -14,7 +15,7 @@ export function Accounts() {
         <span className="tracking-[-0.5px] text-white block">Saldo total</span>
         <div className="flex items-center gap-2">
           <strong className="text-2xl tracking-[-1px] text-white">
-            R$ 1000,00
+            {formatCurrency(1000.23)}
           </strong>
           <button className="w-8 h-8 flex items-center justify-center">
             <EyeIcon open />
--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -5,10 +5,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { MONTHS } from "../../../../../app/config/constants";
 import { SliderOption } from "./SliderOption";
 import { SliderNavigation } from "./SliderNavigation";
+import { formatCurrency } from "../../../../../app/utils/formatCurrency";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
 
 export function Transactions() {
   return (
-    <div className="bg-gray-100 rounded-2xl w-full h-full p-10">
+    <div className="bg-gray-100 rounded-2xl w-full h-full p-10 flex flex-col">
       <header>
         <div className="flex items-center justify-between">
           <button className="flex items-center gap-2">
@@ -44,8 +46,37 @@ export function Transactions() {
         </Swiper>
       </div>
 
-      <div className="mt-4">
-        Conteúdo
+      <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="expense" />
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+              <span className="text-sm text-gray-600">04/06/2024</span>
+            </div>
+          </div>
+
+          <span className="text-red-800 tracking-[-0.5px] font-medium">
+            - {formatCurrency(123)}
+          </span>
+        </div>
+
+        <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
+          <div className="flex-1 flex items-center gap-3">
+            <CategoryIcon type="income" />
+
+            <div>
+              <strong className="font-bold tracking-[-0.5px] block">Almoço</strong>
+              <span className="text-sm text-gray-600">04/06/2024</span>
+            </div>
+          </div>
+
+          <span className="text-green-800 tracking-[-0.5px] font-medium">
+            {formatCurrency(123)}
+          </span>
+        </div>
+
       </div>
     </div>
   );
